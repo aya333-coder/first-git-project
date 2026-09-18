2d4977a39c8b60859604fbf7367d7f5890d4f770
aya333-coder <ayaettaqy28@gmail.com>
2026-09-18 20:41:12 +0100

    initial commit

---
+++ challengc2.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+let lettre = prompt("Entrez une lettre :");
+switch(lettre) {
+    case "a":
+        case "e":
+            case "u":
+                case "o":
+                    case "i":
+                        case "u":
+                            case "y":
+console.log("C'est une voyelle");
+        break;
+        default:
+        console.log("Ce n'est pas une voyelle");
+}
