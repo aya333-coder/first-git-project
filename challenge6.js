ad0cafde73f1d4e9b545fe4928ec30f7b5a2f013
aya333-coder <ayaettaqy28@gmail.com>
2026-09-18 09:21:28 +0100

    initial commit

---
+++ challenge6.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+let a=parseFloat(prompt("entrer le nombre a:"));
+let b=parseFloat(prompt("entrer le nombre b:"));
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
