208c4293d78b5614fb38fa24515331127710e1b4
aya333-coder <ayaettaqy28@gmail.com>
2026-09-21 13:31:09 +0100

    initial commit

--- challeng11t.js
+++ challeng11t.js
@@ -1,6 +1,10 @@
 
 let languages=['HTML','javascript','pythone' ,'c']
+languages[2]="Php"
+languages.push('CSS')
 console.log (languages.length)
 console.log(languages)
 languages[2]="Php"
+console.log(languages)
+languages.push('CSS')
 console.log(languages)
