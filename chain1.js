5a2b24fa48fa0fa292377c2969eac06c23d969d7
aya333-coder <ayaettaqy28@gmail.com>
2026-09-21 17:53:06 +0100

    initial commit

---
+++ chain1.js
@@ -0,0 +1,3 @@
+const prompt = require("prompt-sync")();
+let texte=prompt('entrez un texte:');
+console.log('le texte que vous avez entrez:'+ texte)
--- challeng12t.js
+++ challeng12t.js
@@ -1,1 +1,2 @@
-let number=[]
+let number=[1,2,3,5,7,8,10,12,18]
+if ( nombre % 2===0){
