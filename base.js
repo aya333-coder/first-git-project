0346e6be80701d8740358e32e2633c2496c01842
aya333-coder <ayaettaqy28@gmail.com>
2026-09-22 11:13:36 +0100

    initial commit

---
+++ base.js
@@ -0,0 +1,9 @@
+let age = 20;
+
+if (age >= 18) {
+    console.log("adult");
+} else {
+    console.log("minor");
+}
+
+
---
+++ objets1.js
@@ -0,0 +1,9 @@
+let personne={
+    nom:"ettaqy",
+    prenom:"aya",
+    age:19
+        
+    }
+    console.log(personne.nom);
+    console.log(personne.prenom);
+    console.log(personne.age);
