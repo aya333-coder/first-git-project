0dc8cbe5bc1a5743c9dc3dcee99aa6931965049d
aya333-coder <ayaettaqy28@gmail.com>
2026-09-22 12:31:04 +0100

    initial commit

--- base.js
+++ base.js
@@ -1,9 +1,14 @@
-let age = 20;
+const student={
+    nam:"aya",
+    age:19,
+    ville:"safi",
+    note:15
+};
+student.note=17
+delete student.ville
+console.log(student.nam);
+console.log(student.age);
+console.log(student.note);
 
-if (age >= 18) {
-    console.log("adult");
-} else {
-    console.log("minor");
-}
 
 
